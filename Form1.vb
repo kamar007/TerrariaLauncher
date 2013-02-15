05960930fc20e0f7b274418b981931bed9fa2b1a
KaMaR <kamar007@o2.pl>
2013-02-16 00:05:32 +0100

    git remote add origin https://github.com/kamar007/Updater.git git push -u origin master

---
+++ Form1.vb
@@ -0,0 +1,63 @@
+﻿Public Class Form1
+  
+  
+    Private Sub Button1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        If (System.IO.File.Exists("TerrariaOriginalBackup.exe")) Then 'sprawdza czy istnieje plik
+            Process.Start("TerrariaOriginalBackup.exe") 'jeśli istnieje uruchom
+        Else
+            MsgBox("Nie znalazłem gry") 'jeśli nie istnieje pokaz komunikat
+        End If
+
+    End Sub
+
+
+
+    Private Sub Button3_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button3.Click
+
+        If (System.IO.File.Exists("Terrariapl.exe")) Then 'sprawdza czy istnieje plik
+            Process.Start("Terrariapl.exe") 'jeśli istnieje uruchom
+        Else
+            MsgBox("Nie znalazłem gry") 'jeśli nie istnieje pokaz komunikat
+        End If
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+
+    Private Sub Button2_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button2.Click
+        End
+    End Sub
+
+    Private Sub WyjdźToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles WyjdźToolStripMenuItem.Click
+        End
+    End Sub
+
+    Private Sub SzukajAktualizcjiToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SzukajAktualizcjiToolStripMenuItem.Click
+        If (System.IO.File.Exists("Aktualizator.exe")) Then 'sprawdza czy istnieje plik
+            Process.Start("Aktualizator.exe") 'jeśli istnieje uruchom
+            End
+        Else
+            MsgBox("Nie znalazłem Programu") 'jeśli nie istnieje pokaz komunikat
+        End If
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        If (System.IO.File.Exists("tconfig.exe")) Then 'sprawdza czy istnieje plik
+            Process.Start("Tconfig.exe") 'jeśli istnieje uruchom
+        Else
+            MsgBox("Nie znalazłem gry") 'jeśli nie istnieje pokaz komunikat
+        End If
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        If (System.IO.File.Exists("TehModApi.exe")) Then 'sprawdza czy istnieje plik
+            Process.Start("TehModApi.exe") 'jeśli istnieje uruchom
+        Else
+            MsgBox("Nie znalazłem gry") 'jeśli nie istnieje pokaz komunikat
+        End If
+    End Sub
+End Class
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Terraria Launcher by KaMaR")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("T-mody")> 
+<Assembly: AssemblyProduct("Terraria Launcher by KaMaR")> 
+<Assembly: AssemblyCopyright("Copyright © T-mody 2013")> 
+<Assembly: AssemblyTrademark("http://t-mody.tk")> 
+
+<Assembly: ComVisible(False)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("cbc30c7c-4d5c-4391-a6b3-d4e8b1292eb8")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("0.0.0.3")> 
+<Assembly: AssemblyFileVersion("0.0.0.3")> 
+
+<Assembly: NeutralResourcesLanguageAttribute("pl-PL")> 
